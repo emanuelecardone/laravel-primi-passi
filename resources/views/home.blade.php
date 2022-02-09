74335369e261f412f647d5f7737360d66bdea176
emanuelecardone <e_m.cardone@yahoo.it>
2022-02-09 04:21:31 +0100

    Fatto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        ul{
+            list-style-type: none;
+        }
+        header{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100px;
+            background-color: black;
+            border-bottom: 3px solid lime;
+        }
+        header ul{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 2rem;
+        }
+        a{
+            text-decoration: none;
+            color: lime;
+            font-weight: bold;
+            text-transform: uppercase;
+        }
+        main{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            gap: 5rem;
+            background-color: black;
+            color: lime; 
+            min-height: calc(100vh - 100px);
+        }
+        main ul{
+            width: 50%;
+            display: flex;
+            flex-direction: column;
+            gap: 2rem;
+            text-align: center;
+        }
+
+    </style>
+</head>
+<body>
+    <header>
+        <ul>
+            @forelse($links as $link)
+                <li>
+                    <a href="{{route($link)}}">{{$link}}</a>
+                </li>
+            @empty 
+                <h2>No links were found</h2>
+            @endforelse
+        </ul>
+    </header>
+
+    <main>
+        <h2>Lorems</h2>
+        <ul>
+            @forelse($texts as $lorem)
+                <li>
+                    <p>{{$lorem}}</p>
+                </li>
+            @empty 
+                <h2>No lorems were found</h2>
+            @endforelse
+        </ul>
+    </main>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+
+    $data = [
+        'links' => [
+            'about',
+            'more info',
+            'contact us'
+        ],
+        'texts' => [
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, consectetur qui molestiae dolorum fugiat quam et possimus iusto quidem repellendus recusandae, impedit ea quos vitae hic, ad assumenda incidunt voluptatibus?',
+            'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore sunt praesentium nulla consequuntur provident. Odio accusamus, doloribus eos nesciunt quis quam quas minima nisi cupiditate atque dolor eaque, dolorum delectus.',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deleniti, dolor cum accusamus, repellendus, blanditiis hic maiores consectetur unde voluptate dignissimos expedita tenetur labore eum molestias doloremque quae nihil! In?',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique voluptatum consequatur dolorem illum. Placeat quos voluptas commodi quasi numquam. Rerum maxime ut tenetur officia dolore, molestias numquam cupiditate veniam ipsam!',
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae labore vel rem voluptatum facilis. Itaque, rem fugiat vitae dicta deleniti soluta? Porro ad dicta itaque repellendus illo tempore error qui!'
+        ]
+    ];
+
+    return view('home', $data);
+})->name('home');
+
+Route::get('/about', function () {
+
+    return view('about');
+
+})->name('about');
+
+Route::get('/more_info', function () {
+
+    return view('moreinfo');
+
+})->name('more info');
+
+Route::get('/contact_us', function () {
+
+    return view('contactus');
+
+})->name('contact us');
